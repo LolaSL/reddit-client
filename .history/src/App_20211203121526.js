dfe3b599c75e7420d68d0b205316a9c49b2591b7
LolaSL <lolashepota@gmail.com>
2021-12-03 13:51:08 +0200

    updating css. files code, js. files Login,PostLoading

---
+++ .history/src/App_20211203121526.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './features/header/Header';
+import './App.css';
+import Home from './features/home/Home';
+// import Subreddits from './features/subreddits/Subreddits';
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <Home />
+      </main>
+      {/* <aside>
+        <Subreddits />
+      </aside> */}
+    </div>
+
+  );
+}
+
+export default App;
