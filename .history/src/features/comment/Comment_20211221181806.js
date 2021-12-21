0001bebe60c4af877534eac531faf7e5b7034a67
LolaSL <lolashepota@gmail.com>
2021-12-21 19:35:21 +0200

    updating code in js and css files

---
+++ .history/src/features/comment/Comment_20211221181806.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import moment from 'moment';
+import ReactMarkdown from 'react-markdown';
+import './Comment.css';
+import Avatar from '../avatar/Avatar';
+import remarkGfm from 'remark-gfm';
+
+
+
+const Comment = (props) => {
+    const { comment } = props;
+    const markdown = comment.body;
+    return (
+        <div className="comment">
+            <div className="comment-metadata">
+                <Avatar name={comment.author} />
+                <p className="comment-author">{comment.author}</p>
+                <p className="comment-created-time">
+                    {moment.unix(comment.created_utc).fromNow()}
+                </p>
+            </div>
+            <div id="comment-body">
+                <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+            </div>
+        </div>
+    );
+};
+
+export default Comment;
